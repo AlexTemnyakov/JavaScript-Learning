11aba9ed17990033a6ef9912596da4681ffc11d6
alextemnyakov <temnyale@fel.cvut.cz>
2021-06-17 18:08:13 +0200

    A solution for the task for the sum of the number in the Fibonacci sequence.

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,14 +1,13 @@
 function showMessage() {
-    alert( 'Hello, world!' )
-    alert( 'Hello, world! Again.' )
+    alert( 'This script will show the answers to the tasks from https://projecteuler.net/archives.' )
 }
 
 /**
- * Returns the sum of the numbers up to the passed maximum that are dividable by the passed dividers.
+ * Returns the sum of the multiples of the passed dividers up to the passed maximum.
  *
  * @param {number} max The maximum number to count up to.
  * @param {Array.<number>} dividers An array of dividers.
- * @return {number} The sum of the numbers that are dividable by the dividers.
+ * @return {number} The sum of the multiples of the dividers up to the passed maximum.
  */
 function calculateSumOfMultiplesUpTo(max, dividers) {
     let sum = 0
@@ -17,6 +16,8 @@ function calculateSumOfMultiplesUpTo(max, dividers) {
         for (let j = 0; j < dividers.length; j++) {
             if (i % dividers[j] === 0) {
                 sum += i
+                // Break, because we don't want to count the current number again.
+                break;
             }
         }
     }
@@ -36,5 +37,34 @@ function calculateSumOfMultiplesUpTo(max, dividers) {
     return sum
 }
 
+/**
+ * Returns the sum of the even numbers in the Fibonacci sequence up to the passed maximum.
+ *
+ * @param {number} max The maximum number in the Fibonacci sequence.
+ * @return {number} The sum of the number of the Fibonacci sequence up to the passed maximum.
+ */
+function calculateSumOfEvenFibonacciNumbersUpTo(max) {
+    let sum = 0
+
+    let f1 = 1
+    let f2 = 1
+
+    while (f1 + f2 < max) {
+        let currentNumber = f1 + f2
+        f1 = f2
+        f2 = currentNumber
+        //alert(currentNumber)
+        if (currentNumber % 2 === 0) {
+            sum += currentNumber
+        }
+    }
+
+    let output = "The sum of the numbers in the Fibonacci sequence up to " + max + " is " + sum
+    alert(output)
+
+    return sum
+}
+
 showMessage();
-calculateSumOfMultiplesUpTo(1000, [3, 5]);
+//calculateSumOfMultiplesUpTo(1000, [3, 5]);
+calculateSumOfEvenFibonacciNumbersUpTo(4000000)
