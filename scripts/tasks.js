3c81bc72df11edd5a487e9cbd474fc1799ac4f8f
alextemnyakov <temnyale@fel.cvut.cz>
2021-06-19 12:01:52 +0200

    The task for the count of letters.

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -592,6 +592,111 @@ LearningTasks.solvePowerDigitSum = function (power) {
     return sum;
 }
 
+LearningTasks.solveNumberLetterCounts = function () {
+    var elementaryNumberToWords = function (n)
+    {
+        if (n >= 20 && n % 10 !== 0) {
+            return elementaryNumberToWords(n - (n % 10)) + " " + elementaryNumberToWords(n % 10);
+        } else {
+            switch (n) {
+                case 0:
+                    return "";
+                case 1:
+                    return "one";
+                case 2:
+                    return "two";
+                case 3:
+                    return "three";
+                case 4:
+                    return "four";
+                case 5:
+                    return "five";
+                case 6:
+                    return "six";
+                case 7:
+                    return "seven";
+                case 8:
+                    return "eight";
+                case 9:
+                    return "nine";
+                case 10:
+                    return "ten";
+                case 11:
+                    return "eleven";
+                case 12:
+                    return "twelve";
+                case 13:
+                    return "thirteen";
+                case 14:
+                    return "fourteen";
+                case 15:
+                    return "fifteen";
+                case 16:
+                    return "sixteen";
+                case 17:
+                    return "seventeen";
+                case 18:
+                    return "eighteen";
+                case 19:
+                    return "nineteen";
+                case 20:
+                    return "twenty";
+                case 30:
+                    return "thirty";
+                case 40:
+                    return "forty";
+                case 50:
+                    return "fifty";
+                case 60:
+                    return "sixty";
+                case 70:
+                    return "seventy";
+                case 80:
+                    return "eighty";
+                case 90:
+                    return "ninety";
+            }
+        }
+    }
+
+    var numberToWords = function (n)
+    {
+        var out = "";
+
+        var i = 0;
+
+        i = Math.floor((n / 100) % 10);
+        if (i !== 0) {
+            out += elementaryNumberToWords(i) + " ";
+            out += "hundred ";
+        }
+
+        i = Math.floor(n % 100);
+        if (out !== "" && i !== 0)
+            out += "and ";
+        out += elementaryNumberToWords(i);
+
+        return out;
+    }
+
+    var countOfLetters = 0;
+
+    for (var i = 1; i < 1000; i++) {
+        var numInWords = numberToWords(i);
+        //console.log(i + " " + numInWords);
+        for (var j = 0; j < numInWords.length; j++) {
+            if (numInWords[j] !== " ") {
+                countOfLetters++;
+            }
+        }
+    }
+
+    var output = "The count of letters in the numbers from 1 to 100 that are written in words is " + countOfLetters + ".";
+    alert(output);
+
+    return countOfLetters;
+}
+
 //LearningTasks.calculateSumOfMultiplesUpTo(1000, [3, 5]);
 //LearningTasks.calculateSumOfEvenFibonacciNumbersUpTo(4000000);
 //LearningTasks.findLargestPrimeFactorOfNumber(600851475143);
@@ -751,4 +856,5 @@ LearningTasks.solvePowerDigitSum = function (power) {
 //     100);
 //LearningTasks.solveLongestCollatzSequence(1000000);
 //LearningTasks.solveLatticePaths(20);
-LearningTasks.solvePowerDigitSum(1000)
+//LearningTasks.solvePowerDigitSum(1000);
+LearningTasks.solveNumberLetterCounts();
